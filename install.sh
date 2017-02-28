9b6024772fdb4359ba25d7e5d5a82ba672df6b87
Archibald Samuel Elliott <t-arelli@microsoft.com>
2017-02-28 09:44:07 -0800

    Limit Clone Depth in builds

--- install.sh
+++ install.sh
@@ -21,23 +21,25 @@ fi
 
 cmake --version
 
+CLONE_DEPTH=50
+
 # Check out LLVM
 if [ ! -d llvm/.git ]; then
-  git clone  https://github.com/Microsoft/checkedc-llvm llvm
+  git clone --depth ${CLONE_DEPTH} https://github.com/Microsoft/checkedc-llvm llvm
 else
   (cd llvm; git pull)
 fi;
 
 # Check out Clang
 if [ ! -d llvm/tools/clang/.git ]; then
-  (cd llvm/tools; git clone https://github.com/Microsoft/checkedc-clang clang)
+  (cd llvm/tools; git clone --depth ${CLONE_DEPTH} https://github.com/Microsoft/checkedc-clang clang)
 else
   (cd llvm/tools/git; git pull)
 fi
 
 # Check out Checked C Tests
 if [ ! -d llvm/projects/checkedc-wrapper/checkedc/.git ]; then
-  (cd llvm/projects/checkedc-wrapper; git clone https://github.com/Microsoft/checkedc checkedc)
+  (cd llvm/projects/checkedc-wrapper; git clone --depth ${CLONE_DEPTH} https://github.com/Microsoft/checkedc checkedc)
 else
   (cd llvm/projects/checkedc-wrapper/checkedc; git pull)
 fi
