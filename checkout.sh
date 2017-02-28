ba4349a867aa0ae35700ef8929f8f0c6eb0cb065
Archibald Samuel Elliott <t-arelli@microsoft.com>
2017-02-28 15:53:35 -0800

    Improve Error Handling

--- checkout.sh
+++ checkout.sh
@@ -4,7 +4,7 @@ set -ue
 set -o pipefail
 
 # If we're on travis, set BUILD_OS_NAME to be cross-build-system
-if [ -n "${TRAVIS_OS_NAME}" ]; then
+if [ -n "${TRAVIS_OS_NAME:-}" ]; then
   export BUILD_OS_NAME=$TRAVIS_OS_NAME
 fi
 
