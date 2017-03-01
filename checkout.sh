982ff09327f75ac7b453d3e1264abe8c31b2df06
Archibald Samuel Elliott <t-arelli@microsoft.com>
2017-03-01 11:06:43 -0800

    Add --no-progress-bar to llvm-lit args

--- checkout.sh
+++ checkout.sh
@@ -49,7 +49,12 @@ mkdir -p llvm.build
 
 # Run CMake for llvm.build (cached), uses cmake setup in install.sh
 (cd llvm.build;
-$CMAKE_OUR_BIN -G "Unix Makefiles" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=On ../llvm)
+$CMAKE_OUR_BIN -G "Unix Makefiles" \
+  -DLLVM_TARGETS_TO_BUILD="X86" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_ASSERTIONS=On \
+  -DLLVM_LIT_ARGS="-sv --no-progress-bar --show-unsupported" \
+  ../llvm)
 
 # Install lnt into the virtualenv we set up in install.sh
 (cd lnt;
