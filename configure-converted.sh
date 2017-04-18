f7f9e52d30312d6abdd3f77b0f8f0fb4807c4d53
Sam Elliott <ashe2@cs.washington.edu>
2017-04-18 12:44:27 -0700

    Update to Current Master

--- configure-converted.sh
+++ configure-converted.sh
@@ -2,6 +2,6 @@
 
 # TODO: Update these SHAs
 export CHECKEDC_CLANG_HEAD=8ad4042b757e461488d1c92ebec25753a8a518ae
-export CHECKEDC_TESTS_HEAD=ab6ba69df3a2ba9fac785fe346ff6088a8e0c947
+export CHECKEDC_TESTS_HEAD=bdc6e7f81aca00eddeedb940c3e81ae1f7406d7b
 
 export EXTRA_TEST_ARGS="--cflags -fcheckedc-extension"
