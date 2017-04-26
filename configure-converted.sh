c8a0dfd5ccba4c1ab6ee64b71d551e86691c65b0
Sam Elliott <sam@lenary.co.uk>
2017-04-25 18:08:16 -0700

    Update Converted Test Suite SHA

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=7150247664d3ead1ce43cc0bbc57d1331f107baf
-export CHECKEDC_TESTS_HEAD=8a195fab046ad85a28975fe577919e20256483dd
+export CHECKEDC_TESTS_HEAD=cffd9d802aabda3ea8e50e48039f91c413ef598f
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
