7d83ecc4f3e7011c26bc7fe3f10ff2b09873f0cb
Sam Elliott <ashe2@cs.washington.edu>
2017-05-14 16:44:24 -0700

    Update SHAs for more recent checkedc-clang and test suite changes

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CHECKEDC_CLANG_HEAD=7150247664d3ead1ce43cc0bbc57d1331f107baf
-export CHECKEDC_TESTS_HEAD=cffd9d802aabda3ea8e50e48039f91c413ef598f
+export CHECKEDC_CLANG_HEAD=99cd57abcd044ad8c176bbff3591139a25a7c53a
+export CHECKEDC_TESTS_HEAD=b995113921af682494ec0aa2cd4c8d6987fc2c24
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
