086268ff0e30a33c761dfc4f3dbdb8fef1363540
Sam Elliott <ashe2@cs.washington.edu>
2017-08-15 13:28:21 -0700

    WIP

--- checkout.sh
+++ checkout.sh
@@ -37,7 +37,7 @@ clone_or_update llvm/tools/clang https://github.com/Microsoft/checkedc-clang ${C
 clone_or_update llvm/projects/checkedc-wrapper/checkedc https://github.com/Microsoft/checkedc.git ${CHECKEDC_SPEC_HEAD:-master}
 
 # Check out Test Suite
-clone_or_update llvm-test-suite https://github.com/Microsoft/checkedc-llvm-test-suite ${CHECKEDC_TESTS_HEAD:-master}
+clone_or_update llvm-test-suite https://github.com/lenary/checkedc-llvm-test-suite ${CHECKEDC_TESTS_HEAD:-master}
 
 set +ue
 set +o pipefail
