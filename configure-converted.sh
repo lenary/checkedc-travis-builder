718bf0ba878518b36bfff95ebac39eb5d476ece4
Sam Elliott <ashe2@cs.washington.edu>
2017-08-15 17:23:14 -0700

    WIP

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=0121c97c5d372f5657cedba8395ac8d7ea9d9ee1
-export CHECKEDC_TESTS_HEAD=64200b474e3703b5b51eea56bba2c70ccfa86c1b
+export CHECKEDC_TESTS_HEAD=8d76f1e03b60b194ad8e6b3953c9fe8f5e357dd3
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
