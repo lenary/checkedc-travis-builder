f3d0f11590853b91a3d6d3eb157b2064fb5d3b43
Sam Elliott <ashe2@cs.washington.edu>
2017-08-16 13:43:25 -0700

    Update anagram source

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=0121c97c5d372f5657cedba8395ac8d7ea9d9ee1
-export CHECKEDC_TESTS_HEAD=c5ff8c3f68875d9173d806420234099acef1df67
+export CHECKEDC_TESTS_HEAD=dee709367d7091c0a69e745368b23630556cdf75
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
