9d2959b4c9d199f51d0979bef0d891afea823bb9
Sam Elliott <ashe2@cs.washington.edu>
2017-08-28 13:37:01 -0700

    Update anagram sha

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=e285764595149855fd96ee19f9d3d9181b48d05b
-export CHECKEDC_TESTS_HEAD=e8d43a023207ce751f1e8e8aecd43eaeddc2c41c
+export CHECKEDC_TESTS_HEAD=70a2dace9cfc45efe1a12f201f66fb49bda4f34f
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
