d834b264d8a42bdca77113258b65e1785ccb8f62
Sam Elliott <ashe2@cs.washington.edu>
2017-08-28 13:39:42 -0700

    SHA

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=e285764595149855fd96ee19f9d3d9181b48d05b
-export CHECKEDC_TESTS_HEAD=64200b474e3703b5b51eea56bba2c70ccfa86c1b
+export CHECKEDC_TESTS_HEAD=32b68013a3b92b814ab2e8be6f38e118c0a56a88
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
