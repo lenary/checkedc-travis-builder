c4015f22cafb691370f41140ce0cf8cd0c49564d
Sam Elliott <ashe2@cs.washington.edu>
2017-09-07 15:54:08 -0700

    Update to hand-optimised em3d

--- configure-converted.sh
+++ configure-converted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export CHECKEDC_CLANG_HEAD=2a168085a3e7b1fafddba32e96d26f2c7700b167
-export CHECKEDC_TESTS_HEAD=32b68013a3b92b814ab2e8be6f38e118c0a56a88
+export CHECKEDC_TESTS_HEAD=86265e553d61f08a6e89187cb267a9c5ca3ee3f5
 
 export EXTRA_TEST_ARGS="${EXTRA_TEST_ARGS} --cflags -fcheckedc-extension"
